8a1da3f3b8e60c51bda6d15d52e05974e5920de2
itsmanjeet <itsmanjeet1998@gmail.com>
2020-01-02 17:03:02 +0000

    initialized recover - a system recovery tool

---
+++ api/func.hh
@@ -0,0 +1,19 @@
+#ifndef __FUNC_HH__
+#define __FUNC_HH__
+#include <releax/app.hh>
+
+int 
+func_recover(App* app) 
+{
+
+    return 0;
+}
+
+int 
+func_reset(App* app)
+{
+
+    return 0;
+}
+
+#endif
---
+++ src/main.cc
@@ -0,0 +1,59 @@
+#include <func.hh>
+#include <releax/filesys.hh>
+#include <releax/config.hh>
+
+#define loc_config "config.cfg"
+#define global_config "/etc/recover.cfg"
+
+int 
+main(int    ac,
+     char** av)
+{
+    Config *cfg;
+    
+    if (filesys::exist(loc_config)) {
+        cfg = new Config(loc_config);
+    } else if (filesys::exist(global_config)) {
+        cfg = new Config(loc_config);
+    } else {
+        std::cout << "No Configuration file exist" << std::endl;
+        return -2;
+    }
+
+
+
+    auto app = new App();
+    app->name("recover")
+       ->version(0.1)
+       ->release('a')
+       ->desc("System recovery tool")
+       
+       ->author(
+           "Manjeet Saini",
+           "itsmanjeet1998@gmail.com",
+           "Lead Developer"
+        )
+        
+        ->sub(
+            "sysimage",
+            "Recover system from backup image ( replace /usr )",
+            "-i [ImageLoc]",
+            func_recover
+        )
+        
+        ->sub(
+            "reset",
+            "Reset System configurations ( delete ~/.* , /etc, /var )",
+            "\t\t",
+            func_reset
+        );
+
+    int status = app->execute(ac, av);
+    if (status != 0) {
+        std::cout << "Error occured " << status << std::endl;
+    }
+
+    delete app;
+    delete cfg;
+    return status;
+}
